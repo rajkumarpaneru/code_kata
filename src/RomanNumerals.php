6af33dddc7c0fa6d91c34155a41792a3dcf5767e
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2023-01-08 15:36:27 +0545

    test to implement number to roman

--- src/RomanNumerals.php
+++ src/RomanNumerals.php
@@ -4,17 +4,34 @@ namespace App;
 
 class RomanNumerals 
 {
-    public function toRoman($number)
+    const NUMERALS = [
+        'M' => 1000,
+        'CM' => 900,
+        'D' => 500,
+        'CD' => 400,
+        'C' => 100,
+        'XC' => 90,
+        'L' => 50,
+        'XL' => 40,
+        'X' => 10,
+        'IX' => 9,
+        'V' => 5,
+        'IV' => 4,
+        'I' => 1,
+    ];
+
+    public static function toRoman($number)
     {
-        $roman = '';
-        
-        if($number == 1)
-            $roman = 'I';
+        $result = '';
 
-        if($number == 2)
-            $roman = 'II';
+        foreach(static::NUMERALS as $numeral => $arabic){
+            while($number >= $arabic){
+                $result .= $numeral;
 
+                $number -= $arabic;
+            }        
+        }
 
-        return $roman;
+        return $result;
     }
 }
--- tests/RomanNumeralsTest.php
+++ tests/RomanNumeralsTest.php
@@ -8,35 +8,36 @@ class RomanNumeralsTest extends TestCase
  
     /** 
      * @test
+     * @dataProvider numbers
     */
 
-     public function it_returns_roman_number_for_1()
+     public function it_returns_roman_number($number, $expected)
      {
-        $roman = new RomanNumerals();
-
-        $this->assertEquals('I',$roman->toRoman(1));
+        $this->assertEquals($expected,RomanNumerals::toRoman($number));
      }
 
-    /** 
-     * @test
-    */
-
-    public function it_returns_roman_number_for_2()
+    public function numbers()
     {
-       $roman = new RomanNumerals();
-
-       $this->assertEquals('II',$roman->toRoman(2));
+        return [
+            [1, 'I'],
+            [2, 'II'],
+            [3, 'III'],
+            [4, 'IV'],
+            [5, 'V'],
+            [6, 'VI'],
+            [7, 'VII'],
+            [8, 'VIII'],
+            [9, 'IX'],
+            [10, 'X'],
+            [40, 'XL'],
+            [50, 'L'],
+            [90, 'XC'],
+            [100, 'C'],
+            [400, 'CD'],
+            [500, 'D'],
+            [900, 'CM'],
+            [1000, 'M'],
+            [3999, 'MMMCMXCIX']
+        ];
     }
-
-    /** 
-     * @test
-    */
-
-    public function it_returns_roman_number_for_3()
-    {
-       $roman = new RomanNumerals();
-
-       $this->assertEquals('III',$roman->toRoman(3));
-    }
-
 }
